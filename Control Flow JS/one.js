ecffde6ed4ff65148b44902d657dee853532e507
fawadsw20 <fawadhassan.sw1997@gmail.com>
2024-07-15 17:27:30 +0500

    Add files via upload

---
+++ Control Flow JS/one.js
@@ -0,0 +1,41 @@
+// if statement
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// for value checking we used == double Equal sign
+
+if(2 == "2"){
+//  console.log("Executed");
+}
+
+// for strict check we used === equal sign to check the data type also 
+
+if (2 === "2"){
+
+//     console.log("Executed");
+}
+
+
+// for strict checking anti pattern we use !== exclamatory
+
+if ( 2 !== 3){
+
+    // console.log ("executed");
+}
+
+if ( temperature < 50){
+
+    console.log("less than 50");
+}
+
+else if (temperature > 40){
+
+    console.log("greater than 40"); 
+}
+
+
+
+
+// assignment operator : =
+// <, >, <=, >=, ==, ===, !=,  !==
